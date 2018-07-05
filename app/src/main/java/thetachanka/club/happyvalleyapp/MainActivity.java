8f67095578d498ea3eb2728e83be5a31ef6b8780
DoneRaging <billing.caleb14@gmail.com>
2018-07-05 22:19:37 +0930

    WebPage viewing functional
    Added the WebView widget and relevant code for it to view the webpage.
    Updated AndroidManifest.xml to only allow portrait orientation.

--- app/src/main/java/thetachanka/club/happyvalleyapp/MainActivity.java
+++ app/src/main/java/thetachanka/club/happyvalleyapp/MainActivity.java
@@ -1,17 +1,15 @@
 package thetachanka.club.happyvalleyapp;
 
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.webkit.WebView;
 import android.widget.TextView;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 
 public class MainActivity extends AppCompatActivity {
     TextView text;
+    String words;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,41 +19,11 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().setTitle("Timetable 2018");
         setContentView(R.layout.activity_main);
-        readHTML();
+
+        String url = "http://thetachanka.club:5000";
+        WebView view = findViewById(R.id.web_view);
+        view.getSettings().setJavaScriptEnabled(true);
+        view.loadUrl(url);
     }
 
-    /**
-     * Executes the ConnectToWebpage onPostExecute method to read text from the webpage
-     */
-    public void readHTML() {
-        new ConnectToWebpage().execute();
-    }
-
-    public class ConnectToWebpage extends AsyncTask<Void, Void, Void> {
-        String words;
-
-        /**
-         * Reads the text from http://thetachanka.club:5000
-         * @param params
-         * @return null
-         */
-        @Override
-        protected Void doInBackground(Void... params) {
-            try {
-                Document doc = Jsoup.connect("http://thetachanka.club:5000").get();
-                words = doc.text();
-            } catch (Exception e) {text.setText("unable to connect");}
-            return null;
-        }
-
-        /**
-         * Sets the text of the TextView to the webpages text
-         * @param aVoid
-         */
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            text.setText(words);
-        }
-    }
 }
