f0cac05e465d0f54d86f36386eb75e428d1e63cd
DoneRaging <billing.caleb14@gmail.com>
2018-07-09 13:13:26 +0930

    Visual Update
    changed and added new colours to colors.xml
    Added the PinView to the project for the login_page.xml for a pin input instead of a password

--- app/src/main/java/thetachanka/club/happyvalleyapp/LoginPage.java
+++ app/src/main/java/thetachanka/club/happyvalleyapp/LoginPage.java
@@ -4,30 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.EditText;
+
+import com.goodiebag.pinview.Pinview;
 
 public class LoginPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().setTitle("Happy Valley Youth Group 2018");
         setContentView(R.layout.login_page);
-    }
-
-    public void login(View v) {
-        EditText passField = findViewById(R.id.password_field);
-        String passText = passField.getText().toString();
-        if (passText.equals("hvyg18")) {
-            findViewById(R.id.incorrect_password).setVisibility(View.INVISIBLE);
-            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-        } else {
-            findViewById(R.id.incorrect_password).setVisibility(View.VISIBLE);
-        }
+        Pinview pinview = findViewById(R.id.pin);
+        pinview.setPinViewEventListener(new Pinview.PinViewEventListener() {
+            @Override
+            public void onDataEntered(Pinview pinview, boolean b) {
+                if (pinview.getValue().equals("1234")) {
+                    findViewById(R.id.incorrect_password).setVisibility(View.INVISIBLE);
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                } else {
+                    findViewById(R.id.incorrect_password).setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/thetachanka/club/happyvalleyapp/MainActivity.java
+++ app/src/main/java/thetachanka/club/happyvalleyapp/MainActivity.java
@@ -2,28 +2,20 @@ package thetachanka.club.happyvalleyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
 import android.webkit.WebView;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView text;
-    String words;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().setTitle("Timetable 2018");
         setContentView(R.layout.activity_main);
 
         String url = "http://thetachanka.club:5000";
-        WebView view = findViewById(R.id.web_view);
-        view.getSettings().setJavaScriptEnabled(true);
-        view.loadUrl(url);
+        WebView webview = findViewById(R.id.web_view);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl(url);
     }
 
 }
