84e1fc961de59822156ae812c365d2d80f8ba6ee
DoneRaging <billing.caleb14@gmail.com>
2018-07-10 15:23:57 +0930

    Extended Functionalities
    Added round_button.xml to drawable
    Created the App icon
    Added new views
        - Next Event
        - Timetable
        - Images

--- app/src/main/java/thetachanka/club/happyvalleyapp/MainActivity.java
+++ app/src/main/java/thetachanka/club/happyvalleyapp/MainActivity.java
@@ -2,7 +2,9 @@ package thetachanka.club.happyvalleyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +14,51 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Timetable 2018");
         setContentView(R.layout.activity_main);
 
-        String url = "http://thetachanka.club:5000";
-        WebView webview = findViewById(R.id.web_view);
-        webview.getSettings().setJavaScriptEnabled(true);
-        webview.loadUrl(url);
+        String timetable_url = "http://thetachanka.club:5000/timetable/";
+        String images_url = "http://thetachanka.club:5000/images/";
+        String event_url = "http://thetachanka.club:5000/next-event/";
+
+        WebView webTimetable = findViewById(R.id.web_timetable);
+        webTimetable.getSettings().setJavaScriptEnabled(true);
+        webTimetable.loadUrl(timetable_url);
+
+        WebView webImages = findViewById(R.id.web_images);
+        webImages.getSettings().setJavaScriptEnabled(true);
+        webImages.loadUrl(images_url);
+
+        WebView webEvent = findViewById(R.id.web_event);
+        webEvent.getSettings().setJavaScriptEnabled(true);
+        webEvent.loadUrl(event_url);
+
+        Button timetableButton = findViewById(R.id.timetable_button);
+        timetableButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.web_timetable).setVisibility(View.VISIBLE);
+                findViewById(R.id.web_images).setVisibility(View.INVISIBLE);
+                findViewById(R.id.web_event).setVisibility(View.INVISIBLE);
+            }
+        });
+
+        Button imagesButton = findViewById(R.id.images_button);
+        imagesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.web_timetable).setVisibility(View.INVISIBLE);
+                findViewById(R.id.web_images).setVisibility(View.VISIBLE);
+                findViewById(R.id.web_event).setVisibility(View.INVISIBLE);
+            }
+        });
+
+        Button nextEventButton = findViewById(R.id.next_event_button);
+        nextEventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.web_timetable).setVisibility(View.INVISIBLE);
+                findViewById(R.id.web_images).setVisibility(View.INVISIBLE);
+                findViewById(R.id.web_event).setVisibility(View.VISIBLE);
+            }
+        });
     }
 
 }
